f51668e09a75eeacaef25613bd7db84a8fcd414c
Prof. Elias Carvalho <ecacarva2@gmail.com>
2012-05-27 23:53:48 -0300

    Update comments

--- analysis1.R
+++ analysis1.R
@@ -13,6 +13,7 @@ setwd("C:\\GitHub\\UEM_2x2\\")
 # data1=reading the first database (work professor x student)
 # Header for data1 ####################################
 # GRO = Group, ENC=Encounter with mentor and researcher
+#       1=Control, 2=Template, 3=Swarm, 4=Template+Swarm
 # EXP=Explanation from mentor to researcher, QOW=Quality of writing
 # SOR=satisfaction of Student, CWR=Communication from mentor with Student
 # reading the second database (introdcution corrections)
